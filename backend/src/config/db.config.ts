6f7027a917e378b0b8775d9f387ae560cf380f21
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2022-07-05 14:12:29 -0400

    added a session store using connect-mongo package

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
+import MongoStore from 'connect-mongo';
 import multer from 'multer';
 import logging from "../lib/logging";
 dotenv.config();
@@ -20,12 +21,18 @@ const MONGO_OPTIONS = {
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME || 'database';
+const MONGO_URL = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`
+const SESSION_STORE = MongoStore.create({
+    mongoUrl: MONGO_URL,
+    collectionName: 'sessions'
+});
 
 const MONGO = {
     host: MONGO_HOST,
     options: MONGO_OPTIONS,
-    url: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`,
-    upload: UPLOADS
+    url: MONGO_URL,
+    upload: UPLOADS,
+    sessionStore: SESSION_STORE
 };
 
 mongoose.connect(MONGO.url, MONGO.options)
--- backend/src/middlewares/session.middleware.ts
+++ backend/src/middlewares/session.middleware.ts
@@ -3,8 +3,10 @@ import config from "../config/general.config";
 
 const sessionMiddleware = session({
     secret: config.server.sessionSecret,
+    store: config.mongo.sessionStore,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: { secure: true }
 });
 
 export default sessionMiddleware;
