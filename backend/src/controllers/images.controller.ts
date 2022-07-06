66b856ee4d4de09058349e528ec9f7f8da12f93e
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2022-07-06 17:43:06 -0400

    added controller template for image routes

---
+++ backend/src/controllers/images.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+const NAMESPACE = "Images Controller";
+
+export const addImage: RequestHandler = (req: Request, res: Response, next: NextFunction) => {};
+
+export const deleteImage: RequestHandler = (req: Request, res: Response, next: NextFunction) => {};
