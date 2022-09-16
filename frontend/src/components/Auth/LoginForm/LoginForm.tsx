cb9b340bfa33ed818d59e92b81a66947bb7ef72d
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2022-09-15 22:48:13 -0400

    Login form

--- frontend/src/components/Auth/LoginForm/LoginForm.tsx
+++ frontend/src/components/Auth/LoginForm/LoginForm.tsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 import { Button, Stack, TextField } from '@mui/material';
+import useStyles from "./styles";
 
 interface LoginProps {}
 
 const LoginForm: React.FC<LoginProps> = () => {
     const [loginInfo, setLoginInfo] = useState({ username: '', password: '' });
+    const classes = useStyles();
 
     return (
-        <div>
-            <form id="login-form">
-                <Stack spacing={2} maxWidth={0.2}>
-                    <TextField label="Username" required />
-                    <TextField label="Password" required />
-                    <Button variant="contained">Login</Button>
-                </Stack>
-            </form>
-        </div>
+        <Stack sx={{ boxShadow: 1 }}>
+            <div className={classes.loginFormDiv}>
+                <form className={classes.loginForm}>
+                    <Stack spacing={2}>
+                        <TextField label="Username" required />
+                        <TextField label="Password" required />
+                        <Button variant="contained">Login</Button>
+                    </Stack>
+                </form>
+            </div>
+        </Stack>
     );
 };
 
--- frontend/src/components/Auth/LoginForm/styles.tsx
+++ frontend/src/components/Auth/LoginForm/styles.tsx
@@ -2,13 +2,23 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        div: {
+        loginFormDiv: {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
             textAlign: 'center',
             minHeight: '80vh'
-        }
+        },
+
+        loginForm: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            maxWidth: 'fit-content',
+            padding: '2em',
+            // border: 'solid 1px silver',
+            // borderRadius: '5px'
+        },
     })
 );
 
